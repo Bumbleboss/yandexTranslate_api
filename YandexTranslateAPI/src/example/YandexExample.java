105f36492e49efc0dc49ecf9b5f78159fbd4b10d
Bumbleboss <Bumbleboss46@gmail.com>
2018-04-30 12:53:42 +0300

    Renamed example class

--- YandexTranslateAPI/src/example/YandexExample.java
+++ YandexTranslateAPI/src/example/YandexExample.java
@@ -6,13 +6,13 @@ import yandexAPI.YandexLanguage;
 /**
  * @author Bumbleboss
  */
-public class Example {
+public class YandexExample {
 
 	public static void main (String[] args) {
 		
-		YandexAPI api = new YandexAPI("your token");		
+		YandexAPI api = new YandexAPI("trnsl.1.1.20170914T045446Z.7af5b16bee437b84.8b04c10bc54392d10bbc12d9e3ba83b329fe6124");		
 		try {
-			System.out.println(api.getYandexResponse("Hello there!", YandexLanguage.English, YandexLanguage.French)
+			System.out.println(api.getYandexResponse("Hello there!", YandexLanguage.French)
 					.getText().get(0));
 		} catch (YandexException e) {
 			e.printStackTrace();
