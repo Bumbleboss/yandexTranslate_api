5cd0f7a10895dac76b34c7b293a195c3f85247ab
Bumbleboss <Bumbleboss46@gmail.com>
2018-04-30 13:00:31 +0300

    Woops

--- YandexTranslateAPI/src/example/YandexExample.java
+++ YandexTranslateAPI/src/example/YandexExample.java
@@ -10,7 +10,7 @@ public class YandexExample {
 
 	public static void main (String[] args) {
 		
-		YandexAPI api = new YandexAPI("trnsl.1.1.20170914T045446Z.7af5b16bee437b84.8b04c10bc54392d10bbc12d9e3ba83b329fe6124");		
+		YandexAPI api = new YandexAPI("your token");		
 		try {
 			System.out.println(api.getYandexResponse("Hello there!", YandexLanguage.French)
 					.getText().get(0));
